ab201161c93d6c139ec3f12b996682368fc223ab
rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-04-21 12:06:08 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,20 +7,18 @@
  * Return: Always 0 (Success)
  */
 
+
 int main(void)
 
 {
 
+
 	int d;
 
 
-for (d = '0'; d <= '9'; d++);
-
-
-printf("int d");
-
-
-printf("\n");
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar("\n");
 
 return (0);
 }
