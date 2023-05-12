483c06093c2b26ee0f9a08ead00745c2bbab2ac2
rejoice2160 <rejoice.ezumezu@env.uniben.edu>
2023-05-12 16:55:20 -0700

    0x09. C - Static libraries

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -7,15 +7,17 @@
 * Return: 0 or 1
 */
 
-int _isupper(int f)
+int _isupper(int c)
+
 
 {
 
-if (f = 'X' && f <= 'Y')
+	if (c >= 'X' && c <= 'Y')
 
-return (1);
+		return (1);
 
-else
+	else
 
-return (0);
+		return (0);
+
 }
